cbf5a96250366cb50db1b6887d69e5f8c852dea6
“tastethedream” <tastethedream@gmail.com>
2021-05-09 20:49:02 +0100

    background images added and database updated

--- brew_crew/lib/screens/home/settings_form.dart
+++ brew_crew/lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({Key key}) : super(key: key);
@@ -9,7 +13,6 @@ class SettingsForm extends StatefulWidget {
 }
 
 class _SettingsFormState extends State<SettingsForm> {
-
   final _formKey = GlobalKey<FormState>();
   final List<String> sugars = ['0', '1', '2', '3', '4'];
 
@@ -20,51 +23,84 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            'Update your brew settings',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20.0),
-          //dropdown menu
-          DropdownButtonFormField(
-              decoration: textInputDecoration,
-              value: _currentSugars ?? '0',
-              items: sugars.map((sugar){
-                return DropdownMenuItem(
-                  value: sugar,
-                  child: Text('$sugar sugars'),
-                );
-              }).toList(),
-            onChanged: (val) => setState(() => _currentSugars = val),
-          ),
+    final user = Provider.of<User>(context);
 
-          //slider
-          //button
-          RaisedButton(
-            color: Colors.pink[400],
-            child: Text(
-              'Update',
-               style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () async {
-                print(_currentName);
-                print(_currentSugars);
-                print(_currentStrength);
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
+            return Form(
+              key: _formKey,
+
+              // used a wrap here instead of a column to avoid overflow error on bottom sheet
+              child: Wrap(
+                children: [
+                  Text(
+                    'Update your brew settings',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    initialValue: userData.name,
+                    decoration: textInputDecoration,
+                    validator: (val) =>
+                        val.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
+                  ),
+                  SizedBox(height: 20.0),
+                  //dropdown menu
+                  DropdownButtonFormField(
+                    decoration: textInputDecoration,
+                    value: _currentSugars ?? userData.sugars,
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('$sugar sugars'),
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() => _currentSugars = val),
+                  ),
+
+                  //slider
+
+                  Slider(
+                    value: (_currentStrength ?? userData.strength).toDouble(),
+                    activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                    inactiveColor: Colors.brown[_currentStrength ?? userData.strength],
+                    min: 100,
+                    max: 900,
+                    divisions: 8,
+                    onChanged: (val) =>
+                        setState(() => _currentStrength = val.round()),
+                  ),
+                  //button
+                  RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Update',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          await DatabaseService(uid: user.uid).updateUserData(
+                              _currentSugars ?? userData.sugars,
+                              _currentName ?? userData.name,
+                              _currentStrength ?? userData.strength,
+                          );
+                          Navigator.pop(context);
+
+                        }
+                        print(_currentName);
+                        print(_currentSugars);
+                        print(_currentStrength);
+                      }),
+                ],
+              ),
+            );
+          } else {
+            return Loading();
           }
-    ),
-        ],
-      ),
-    );
+        });
   }
 }
-
--- brew_crew/lib/services/database.dart
+++ brew_crew/lib/services/database.dart
@@ -1,12 +1,13 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/user.dart';
 
 class DatabaseService {
   final String uid;
   DatabaseService({this.uid});
 
   // collection reference
-
   final CollectionReference brewCollection =
       Firestore.instance.collection('brews');
 
@@ -19,7 +20,6 @@ class DatabaseService {
   }
 
   // brew list from snapshot
-
   List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot){
     return snapshot.documents.map((doc){
       return Brew(
@@ -30,9 +30,25 @@ class DatabaseService {
     }).toList();
   }
 
+  // userdata from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+    );
+  }
+
   // get brews stream
   Stream<List<Brew>> get brews {
     return brewCollection.snapshots()
     .map(_brewListFromSnapshot);
+  }
+
+  //get user document stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots()
+    .map(_userDataFromSnapshot);
   }
 }
