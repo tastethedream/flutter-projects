47432e876e9a4b012970244b4712855593a54c9d
“tastethedream” <tastethedream@gmail.com>
2021-06-06 21:20:23 +0100

    buttons added to sign in page

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker_flutter_course/widgets/custom_raised_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -9,6 +11,7 @@ class SignInPage extends StatelessWidget {
         elevation: 2.0,
       ),
       body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
 
@@ -17,29 +20,56 @@ class SignInPage extends StatelessWidget {
       padding: EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment. center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Sign In',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w600,
-          ),
-          ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100.0,
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100.0,
-            ),
+          SizedBox(height: 48.0),
+
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
           ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xff334d92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0,),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
+          ),
+
+
         ],
       ),
     );
