efa3b8062e3eb45234ef2b26f088a50584968960
“tastethedream” <tastethedream@gmail.com>
2021-06-08 20:38:24 +0100

    google sign in and sign out added

--- time_tracker_flutter_course/lib/services/auth.dart
+++ time_tracker_flutter_course/lib/services/auth.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 //Define a public interface to support all the authentication methods needed for the project
 abstract class AuthBase {
   User get currentUser;
   Stream<User> authStateChanges();
   Future<User> signInAnonymously();
+  Future<User> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -29,11 +31,38 @@ class Auth implements AuthBase{
   }
 
   // method to sign in with Google
+  @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        ));
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+            message: 'Missing Google ID token',
+            code: 'MISSING_GOOGLE_ID_TOKEN',
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+          message: 'Sign in aborted by user',
+          code: 'ERROR_ABORTED_BY_USER',
+      );
+    }
+  }
   // method to sign in with Facebook
 
- // method to sign out
+ // method to sign out of firebase and Google
   @override
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
     await _firebaseAuth.signOut();
   }
 }
