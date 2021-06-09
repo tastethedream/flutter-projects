70987c15de9ac7ce7ff00f777aba052434a9926b
“tastethedream” <tastethedream@gmail.com>
2021-06-09 15:21:25 +0100

    email passowrd form added and form toggle

---
+++ time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/widgets/form_submit_button.dart';
+
+
+enum EmailSignInFormType {signIn, register}
+
+class EmailSignInForm extends StatefulWidget {
+
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  void _submit() {
+    // print email and password
+    print ('email: ${_emailController.text}, password:${_passwordController.text}');
+  }
+  //toggle between sign in and register
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn ?
+          EmailSignInFormType.register : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn ?
+        'Sign In' : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.signIn ?
+        'Need an account? Register' : 'Have an account? Sign in';
+    return [
+      TextField(
+        controller: _emailController,
+        decoration:
+        InputDecoration(labelText: 'Email', hintText: 'test@test.com'),
+      ),
+      SizedBox(height: 8.0),
+      TextField(
+        controller: _passwordController,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 8.0),
+      FormSubmitButton(
+       text:  primaryText,
+        onPressed: _submit,
+      ),
+      SizedBox(height: 8.0),
+      FlatButton(
+          onPressed: _toggleFormType,
+          child: Text (secondaryText))
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
@@ -6,7 +7,7 @@ import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInPage extends StatelessWidget {
 
-  // anon sign in method
+  // sign in methods
   const SignInPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
 
@@ -35,6 +36,19 @@ class SignInPage extends StatelessWidget {
     }
   }
 
+
+  void _signInWithEmail(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          //fullscreenDialog: true,
+          builder: (context) => EmailSignInPage(),
+      ),
+    );
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,12 +56,12 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: _buildContent(),
+      body: _buildContent(context),
       backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget _buildContent() {
+  Widget _buildContent(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -84,7 +98,7 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Email',
             textColor: Colors.white,
             color: Colors.teal[700],
-            onPressed: () {},
+            onPressed: () =>  _signInWithEmail(context),
           ),
           SizedBox(height: 8.0,),
           Text(
