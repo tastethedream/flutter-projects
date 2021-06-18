e9c738bd48cb444d121d7dd6aa241b84994c87b6
“tastethedream” <tastethedream@gmail.com>
2021-06-18 10:57:54 +0100

    authentication added to the SignInBloc

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 // create class
 class SignInBloc {
+  SignInBloc({@required this.auth});
+  final AuthBase auth;
+
   final StreamController<bool> _isLoadingController = StreamController<bool>();
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
@@ -11,5 +17,23 @@ class SignInBloc {
   }
 
   // method to add values to the stream
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+
+  // Method to handle the different sign in methods
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try{
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+  Future<User> signInAnonymously() async => _signIn(auth.signInAnonymously);
+  Future<User> signInWithGoogle() async => _signIn(auth.signInWithGoogle);
+  Future<User> signInWithFacebook() async => _signIn(auth.signInWithFacebook);
+
+
 }
