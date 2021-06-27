f420c373f500a16893e8947b0482c4b0d74c1fe4
“tastethedream” <tastethedream@gmail.com>
2021-06-27 12:15:29 +0100

    firestore service class added

--- time_tracker_flutter_course/lib/app/home/jobs_page.dart
+++ time_tracker_flutter_course/lib/app/home/jobs_page.dart
@@ -52,10 +52,7 @@ class JobsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: Temp code to access the jobs in the database: delete me
 
-    final database = Provider.of<Database>(context, listen: false);
-    database.jobsStream();
     return Scaffold(
       appBar: AppBar(
         title: Text('Jobs'),
@@ -71,12 +68,32 @@ class JobsPage extends StatelessWidget {
         ],
 
       ),
+      body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: Colors.indigo,
         onPressed: () => _createJob(context),
       ),
     );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+        stream: database.jobsStream(),
+        builder:(context, snapshot) {
+          if (snapshot.hasData) {
+            final jobs = snapshot.data;
+            final children = jobs.map((job) => Text(job.name)).toList();
+            return ListView(children: children);
+
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error'));
+          }
+          return Center(child: CircularProgressIndicator());
+        });
+
   }
 
 }
--- time_tracker_flutter_course/lib/services/database.dart
+++ time_tracker_flutter_course/lib/services/database.dart
@@ -1,48 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/services/api_path.dart';
+import 'package:time_tracker_flutter_course/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
   Stream<List<Job>> jobsStream();
-
 }
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
+  final _service = FirestoreService.instance;
 
-// method to create new job
-  Future<void> createJob(Job job) => _setData(
+  Future<void> createJob(Job job) => _service.setData(
     path: APIPath.job(uid, 'job_abc'),
     data: job.toMap(),
   );
 
-  //method to access the jobs in the database using the map operator to turn a stream of snapshots into a list of jobs
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-            (snapshot) {
-              final data = snapshot.data();
-              return data != null ? Job(
-                name: data['name'],
-                ratePerHour: data['ratePerHour'],
-              ) : null;
-            },
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+    path: APIPath.jobs(uid),
+    builder: (data) => Job.fromMap(data),
+  );
 
-        ));
-  }
-  
-
-
-  //get the database reference from firestore and set the data for that reference
-  Future<void> _setData({String path, Map<String, dynamic> data }) async {
-    final reference = FirebaseFirestore.instance.doc(path);
-    print('$path: $data');
-    await reference.set(data);
-  }
 }
