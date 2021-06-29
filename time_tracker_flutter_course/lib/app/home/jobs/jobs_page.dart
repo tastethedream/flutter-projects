068a138a77251204556d2e11877076beb42a440e
“tastethedream” <tastethedream@gmail.com>
2021-06-29 16:22:20 +0100

    navigate to edit page bug fixed

--- time_tracker_flutter_course/lib/app/home/jobs/jobs_page.dart
+++ time_tracker_flutter_course/lib/app/home/jobs/jobs_page.dart
@@ -74,7 +74,10 @@ class JobsPage extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           backgroundColor: Colors.indigo,
-          onPressed: () => EditJobPage.show(context),
+          onPressed: () => EditJobPage.show(
+              context,
+              database: Provider.of<Database>(context, listen: false),
+          ),
         ),
       );
     }
