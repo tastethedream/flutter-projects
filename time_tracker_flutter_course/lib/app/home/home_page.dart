d682ec899436272315b789b2057b0fa38618e63b
“tastethedream” <tastethedream@gmail.com>
2021-07-01 15:36:10 +0100

    jobs page now displayed when selecting jobs tab on cupertino bar

--- time_tracker_flutter_course/lib/app/home/home_page.dart
+++ time_tracker_flutter_course/lib/app/home/home_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
+import 'jobs/jobs_page.dart';
+
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -12,17 +12,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TabItem _currentTab = TabItem.jobs;
 
+  // map of widget builders for each tab item
+  Map<TabItem, WidgetBuilder> get widgetBuilders {
+    return {
+      TabItem.jobs: (_) => JobsPage(),
+      TabItem.entries: (_) => Container(),
+      TabItem.account: (_) => Container(),
+    };
+  }
+
   // method to switch tab on cupertino bottom bar
   void _select(TabItem tabItem) {
     setState(() => _currentTab = tabItem);
   }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoHomeScaffold(
-        currentTab: _currentTab,
-        onSelectTab: _select,
+      currentTab: _currentTab,
+      onSelectTab: _select,
+      widgetBuilders: widgetBuilders,
     );
   }
-
-
 }
